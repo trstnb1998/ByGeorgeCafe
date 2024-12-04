baff089d09ebc357a8b1f91119abb0544faa1dbf
Tristan <tristanjaybartolome@gmail.com>
2024-12-04 14:13:28 +1100

    navbar component created

---
+++ src/_components_/core/Navbar.tsx
@@ -0,0 +1,14 @@
+import { Typography, Box, List } from "@mui/material"
+
+export default function NavBar() {
+    return (
+        <Box sx={{
+            display: "flex",
+            width: "100%",
+            height: "70px",
+            backgroundColor: "#FF2D2D"
+        }}>
+
+        </Box>
+    )
+}
