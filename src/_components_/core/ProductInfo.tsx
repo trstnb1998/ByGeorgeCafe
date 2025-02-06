858d2cddd8b61a1afd7d52d16471569249f2515e
Tristan <tristanjaybartolome@gmail.com>
2025-02-06 12:50:03 +1100

    all png files changed to uppercase

--- src/_components_/core/ProductInfo.tsx
+++ src/_components_/core/ProductInfo.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 export default function ProductInfo() {
     return (
         <Box sx={{ width: "310px", height: "730px", backgroundColor: "custom.white", marginTop: "5em" }}>
-
+            <Typography>Test</Typography>
         </Box>
     )
 }
